279a5187f9662c32777744f05ad8c5ecf06f2bc5
Ben Bourgart <b.bourgart@fz-juelich.de>
2020-04-15 12:36:07 +0200

    init wiht ones, test broadcasting

--- io.py
+++ io.py
@@ -428,7 +428,7 @@ else:
                 else:
                     var = handle.createVariable(variable, data.dtype.char(), dimension_names, **kwargs)
                 var.set_collective(True)
-                #var[tuple(new_slices)] = np.mgrid[tuple(new_slices)]
+                var[tuple(new_slices)] = np.ones(shape=(1 for _ in new_slices))
                 print('compare shapes:', var[tuple(new_slices)].shape, data.shape, '\nnumpy mgrid:', np.mgrid[tuple(new_slices)].shape, '\nslices', new_slices , flush=True)
                 var[tuple(new_slices)] = (
                     data._DNDarray__array.cpu() if is_split else data._DNDarray__array[slices].cpu()
