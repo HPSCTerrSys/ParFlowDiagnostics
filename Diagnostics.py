0b7527bf88d8be7e4a905f18575eba32c5966153
Stefan Kollet <s.kollet@fz-juelich.de>
2020-05-01 12:32:10 +0200

    Update Diagnostics.py

--- Diagnostics.py
+++ Diagnostics.py
@@ -15,28 +15,29 @@ def printroot(*args, **kwargs):
 class Diagnostics():  # Make this a subclass of ht.DNDarray?
     def __init__(self, Press, Satur, Mask, Permx, Permy, Permz,
                  Alpha, N, Sres, Poro, Sstorage, Mannings,
-                 Slopex, Slopey, Sourcesink, Dx, Dy, Dz, Nx, Ny, Nz):
-        self.Press = Press
-        self.Satur = Satur
-        self.Mask = Mask
-        self.Permx = Permx
-        self.Permy = Permy
-        self.Permz = Permz
-        self.Alpha = Alpha
-        self.N = N
-        self.Sres = Sres
-        self.Poro = Poro
-        self.Sstorage = Sstorage
-        self.Mannings = Mannings
-        self.Slopex = Slopex
-        self.Slopey = Slopey
-        self.Sourcesink = Sourcesink
-        self.Dx = float(Dx)
-        self.Dy = float(Dy)
-        self.Dz = float(Dz)
-        self.Nx = int(Nx)
-        self.Ny = int(Ny)
-        self.Nz = int(Nz)
+                 Slopex, Slopey, Sourcesink, Dx, Dy, Dz, Nx, Ny, Nz, split=None):
+        self.Press      = ht.array(Press,      split=split)
+        self.Satur      = ht.array(Satur,      split=split)
+        self.Mask       = ht.array(Mask,       split=split)
+        self.Permx      = ht.array(Permx,      split=split)
+        self.Permy      = ht.array(Permy,      split=split)
+        self.Permz      = ht.array(Permz,      split=split)
+        self.Alpha      = ht.array(Alpha,      split=split)
+        self.N          = ht.array(N,          split=split)
+        self.Sres       = ht.array(Sres,       split=split)
+        self.Poro       = ht.array(Poro,       split=split)
+        self.Sstorage   = ht.array(Sstorage,   split=split)
+        self.Mannings   = ht.array(Mannings,   split=split)
+        self.Slopex     = ht.array(Slopex,     split=split)
+        self.Slopey     = ht.array(Slopey,     split=split)
+        self.Sourcesink = ht.array(Sourcesink, split=split)
+        self.Dx         = float(Dx)
+        self.Dy         = float(Dy)
+        self.Dz         = float(Dz)
+        self.Nx         = int(Nx)
+        self.Ny         = int(Ny)
+        self.Nz         = int(Nz)
+        self.split      = split
 
     def TotalSubsurfaceStorage(self):
         shape3D = (self.Nx, self.Ny, self.Nz)
