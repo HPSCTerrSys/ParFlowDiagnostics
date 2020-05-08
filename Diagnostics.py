a0770d9bca497e7e1ac2f94919fd7ebbb2b71fda
Stefan Kollet <kollet1@jrl11.jureca>
2020-05-08 15:33:38 +0200

    Expanded Diagnostics

--- Diagnostics.py
+++ Diagnostics.py
@@ -13,7 +13,7 @@ def printroot(*args, **kwargs):
 
 
 class Diagnostics:  # Make this a subclass of ht.DNDarray?
-    def __init__(self, Press, Satur, Mask, Poro, Sstorage, Mannings, Slopex, Slopey, Dx, Dy, Dz, Dzmult, Nx, Ny, Nz):
+    def __init__(self, Press, Satur, Mask, Poro, Sstorage, Mannings, Slopex, Slopey, Dx, Dy, Dz, Dzmult, Nx, Ny, Nz, Split):
         self.Press      = Press
         self.Satur      = Satur
         self.Mask       = Mask
@@ -29,11 +29,11 @@ class Diagnostics:  # Make this a subclass of ht.DNDarray?
         self.Nx         = Nx
         self.Ny         = Ny
         self.Nz         = Nz
-        self.Top        = None
+        self.Split      = Split
 
     def SubsurfaceStorage(self):
         shape3D = (self.Nz, self.Ny, self.Nx)
-        subsurface_storage = ht.zeros(shape3D, split=None)
+        subsurface_storage = ht.zeros(shape3D, split=self.Split)
         for k in range(self.Nz):
             subsurface_storage = self.Satur * self.Poro *  self.Dz * self.Dzmult[k]
             subsurface_storage += self. Press * self.Sstorage * self.Satur * self.Dz * self.Dzmult[k]
@@ -41,43 +41,85 @@ class Diagnostics:  # Make this a subclass of ht.DNDarray?
 
     def VolumetricMoisture(self):
         shape3D = (self.Nz, self.Ny, self.Nx)
-        volumetric_moisture = ht.zeros(shape3D, split=None)
+        volumetric_moisture = ht.zeros(shape3D, split=self.Split)
         volumetric_moisture = self.Satur * self.Poro
         return(volumetric_moisture)
 
     def Toplayer(self):
-        shape2D = (self.Ny, self.Nx)
-        self.Top = ht.zeros(shape2D,split=None)
-        check = ht.full(shape2D,-1.0,split=None)
+        shape1D   = (self.Ny*self.Nx)
+        Topvector = ht.zeros(shape1D,split=self.Split)
+        shape2D   = (self.Ny, self.Nx)
+        Toplayer  = ht.zeros(shape2D,split=self.Split)
+        check     = ht.full(shape2D,-1.0,split=self.Split)
         for k in reversed(range(self.Nz)):
-            self.Top = ht.where((self.Mask[k,:,:]>0.0) & (check<0.0) , k, self.Top)
-            check = ht.where(self.Top>0.0, 0.0, check)
+            Toplayer[:,:] = ht.where((self.Mask[k,:,:]>0.0) & (check<0.0) , k, Toplayer)
+            check = ht.where(Toplayer>0.0, 0.0, check)
+        Topvector = Toplayer.flatten()
+        return(Topvector)
 
-    def OverlandFlow(self):
+    def OverlandFlow(self,top_vector):
         shape2D = (self.Ny,self.Nx)
-        dirx = ht.full(shape2D,-1.0,split=None)
-        diry = ht.full(shape2D,-1.0,split=None)
+        dirx = ht.full(shape2D,-1.0,split=self.Split)
+        diry = ht.full(shape2D,-1.0,split=self.Split)
 
         dirx = ht.where(self.Slopex>0.0, 1.0, dirx)
         diry = ht.where(self.Slopey>0.0, 1.0, diry)
 
         #We need to pick the pressure of the top cell/layer
-        press = ht.zeros(shape2D,split=None)
-        check = ht.full(shape2D,-1.0,split=None)
+        #Don't know how to do that with the top_vector therefore...
+        press = ht.zeros(shape2D,split=self.Split)
+        check = ht.full(shape2D,-1.0,split=self.Split)
         for k in reversed(range(self.Nz)):
             press[:,:] = ht.where((self.Mask[k,:,:]>0.0) & (check<0.0) , self.Press[k,:,:], press)
             check[:,:] = ht.where(press!=0.0, 0.0, check)
 
-
         #We need only the positive pressure values and set the rest to zero, which results in zero overland flow
         press = ht.where(press>0.0, press, 0.0)
 
-        flowx=ht.zeros(shape2D,split=None)
-        flowy=ht.zeros(shape2D,split=None)
+        flowx=ht.zeros(shape2D,split=self.Split)
+        flowy=ht.zeros(shape2D,split=self.Split)
         flowx[:,:] = dirx * (ht.absolute(self.Slopex[0,:,:]))**(1/2)/self.Mannings[0,:,:] * press**(5/3)
         flowy[:,:] = diry * (ht.absolute(self.Slopey[0,:,:]))**(1/2)/self.Mannings[0,:,:] * press**(5/3)
 
         return(flowx, flowy)
+
+    def NetLateralOverlandFlow(self,overland_flow_x,overland_flow_y):
+        shape2D = (self.Ny, self.Nx)
+        Nix = ht.zeros(shape2D, split=self.Split)
+
+        #Calc flow east
+        #ParFlow:ke_[io] = pfmax(qx_[io], 0.0) - pfmax(-qx_[io + 1], 0.0);
+        flow_east = ht.zeros(shape2D, split=self.Split)
+        for i in range (self.Nx-1):
+            flow_east[:,i]  = ht.maximum(overland_flow_x[:,i], Nix[:,i])
+            flow_east[:,i] -= ht.maximum((-1)*overland_flow_x[:,i+1], Nix[:,i+1])
+
+        #Calc flow west
+        #ParFlow:kw_[io] = pfmax(qx_[io - 1], 0.0) - pfmax(-qx_[io], 0.0);
+        flow_west = ht.zeros(shape2D, split=self.Split)
+        for i in range (1,self.Nx):
+            flow_west[:,i]  = ht.maximum(overland_flow_x[:,i-1], Nix[:,i-1])
+            flow_west[:,i] -= ht.maximum((-1)*overland_flow_x[:,i], Nix[:,i])
+
+        #Calc flow north
+        #ParFlow:kn_[io] = pfmax(qy_[io], 0.0) - pfmax(-qy_[io + sy_p], 0.0);
+        flow_north = ht.zeros(shape2D, split=self.Split)
+        for j in range (self.Ny-1):
+            flow_north[j,:]  = ht.maximum(overland_flow_y[j,:], Nix[j,:])
+            flow_north[j,:] -= ht.maximum((-1)*overland_flow_y[j+1,:], Nix[j+1,:])
+
+        #Calc flow south
+        #ParFlow:ks_[io] = pfmax(qy_[io - sy_p], 0.0) - pfmax(-qy_[io], 0.0);
+        flow_south = ht.zeros(shape2D, split=self.Split)
+        for i in range (1,self.Ny):
+            flow_south[j,:]  = ht.maximum(overland_flow_x[j-1,:], Nix[j-1,:])
+            flow_south[j,:] -= ht.maximum((-1)*overland_flow_x[j,:], Nix[j,:])
+
+        #Calc net lateral overland flow for each grid cell, (L/T)
+        #ParFlow: ((ke_[io] - kw_[io]) / dx + (kn_[io] - ks_[io]) / dy
+        net_lateral_overlandflow = (flow_east - flow_west)/self.Dx + (flow_north - flow_south)/self.Dy
+
+        return(net_lateral_overlandflow)
 
 if __name__ == '__main__':
     pass
--- Driver.py
+++ Driver.py
@@ -5,16 +5,17 @@ import os
 from Diagnostics import Diagnostics 
 import IO as io
 
+Split=None
 path = '/p/scratch/cesmtst/esmtst00/cordex3km/'
-press    = io.read_pfb(path + 'cordexIHME.out.press.00000.pfb',None)
-satur    = io.read_pfb(path + 'cordexIHME.out.satur.00000.pfb',None)
-sstorage = io.read_pfb(path + 'cordexIHME.out.specific_storage.pfb',None)
-mask     = io.read_pfb(path + 'cordexIHME.out.mask.pfb',None)
-poro     = io.read_pfb(path + 'cordexIHME.out.porosity.pfb',None)
-mannings = io.read_pfb(path + 'cordexIHME.out.mannings.pfb',None)
-slopex   = io.read_pfb(path + 'ParFlow_MB3km_SLPX_x1592y1544.pfb',None)
-slopey   = io.read_pfb(path + 'ParFlow_MB3km_SLPY_x1592y1544.pfb',None)
-mannings = io.read_pfb(path + 'cordexIHME.out.mannings.pfb',None)
+press    = io.read_pfb(path + 'cordexIHME.out.press.00000.pfb',Split)
+satur    = io.read_pfb(path + 'cordexIHME.out.satur.00000.pfb',Split)
+sstorage = io.read_pfb(path + 'cordexIHME.out.specific_storage.pfb',Split)
+mask     = io.read_pfb(path + 'cordexIHME.out.mask.pfb',Split)
+poro     = io.read_pfb(path + 'cordexIHME.out.porosity.pfb',Split)
+mannings = io.read_pfb(path + 'cordexIHME.out.mannings.pfb',Split)
+slopex   = io.read_pfb(path + 'ParFlow_MB3km_SLPX_x1592y1544.pfb',Split)
+slopey   = io.read_pfb(path + 'ParFlow_MB3km_SLPY_x1592y1544.pfb',Split)
+mannings = io.read_pfb(path + 'cordexIHME.out.mannings.pfb',Split)
 
 Dx = Dy = 3000.0
 Dz = 2.0
@@ -33,7 +34,7 @@ shape4D=(Nt, Nz, Ny, Nx)
 mask = ht.where(mask>0.0, 1.0, mask)
 
 #Initialize Diagnostics class
-diag = Diagnostics(press, satur, mask, poro, sstorage, mannings, slopex, slopey, Dx, Dy, Dz, Dzmult, Nx, Ny, Nz)
+diag = Diagnostics(press, satur, mask, poro, sstorage, mannings, slopex, slopey, Dx, Dy, Dz, Dzmult, Nx, Ny, Nz, None)
 
 #Returns an unmasked 3D field of subsurface storage, (L/L^2)
 subsurface_storage=diag.SubsurfaceStorage()
@@ -45,43 +46,11 @@ volumetric_moisture = diag.VolumetricMoisture()
 subsurface_storage = subsurface_storage * mask
 total_subsurface_storage = ht.sum(subsurface_storage)
 
-#Generate a 2D field with the k-indices of top layer
-diag.Toplayer()
+#Generate a vector with the k-indices of the top layer
+top_vector=diag.Toplayer()
 
 #Calculate overland flow, (L^2/T)
-overland_flow_x,overland_flow_y = diag.OverlandFlow()
+overland_flow_x,overland_flow_y = diag.OverlandFlow(top_vector)
 
-#Calculate net lateral overland flow for each grid cell; should that be a function?
-Nix = ht.zeros(shape2D, split=None)
-
-##Calc flow east
-##ParFlow:ke_[io] = pfmax(qx_[io], 0.0) - pfmax(-qx_[io + 1], 0.0);
-flow_east = ht.zeros(shape2D, split=None)
-for i in range (Nx-1):
-    flow_east[:,i]  = ht.maximum(overland_flow_x[:,i], Nix[:,i]) 
-    flow_east[:,i] -= ht.maximum((-1)*overland_flow_x[:,i+1], Nix[:,i+1])
-
-##Calc flow west
-##ParFlow:kw_[io] = pfmax(qx_[io - 1], 0.0) - pfmax(-qx_[io], 0.0);
-flow_west = ht.zeros(shape2D, split=None)
-for i in range (1,Nx):
-    flow_west[:,i]  = ht.maximum(overland_flow_x[:,i-1], Nix[:,i-1])
-    flow_west[:,i] -= ht.maximum((-1)*overland_flow_x[:,i], Nix[:,i])
-
-##Calc flow north
-##ParFlow:kn_[io] = pfmax(qy_[io], 0.0) - pfmax(-qy_[io + sy_p], 0.0);
-flow_north = ht.zeros(shape2D, split=None)
-for j in range (Ny-1):
-    flow_north[j,:]  = ht.maximum(overland_flow_y[j,:], Nix[j,:])
-    flow_north[j,:] -= ht.maximum((-1)*overland_flow_y[j+1,:], Nix[j+1,:])
-
-##Calc flow south
-##ParFlow:ks_[io] = pfmax(qy_[io - sy_p], 0.0) - pfmax(-qy_[io], 0.0);
-flow_south = ht.zeros(shape2D, split=None)
-for i in range (1,Ny):
-    flow_south[j,:]  = ht.maximum(overland_flow_x[j-1,:], Nix[j-1,:])
-    flow_south[j,:] -= ht.maximum((-1)*overland_flow_x[j,:], Nix[j,:])
-
-##Calc net lateral overland flow for each grid cell, (L/T)
-##ParFlow: ((ke_[io] - kw_[io]) / dx + (kn_[io] - ks_[io]) / dy
-net_lateral_overlandflow = (flow_east - flow_west)/Dx + (flow_north - flow_south)/Dy 
+#Calculate net lateral overland flow
+net_later_overland_flow = diag.NetLateralOverlandFlow(overland_flow_x,overland_flow_y)
