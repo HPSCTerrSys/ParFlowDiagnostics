d73077c93336d87f74c4a0ee7ae23b861bbc7ccb
Ben Bourgart <b.bourgart@fz-juelich.de>
2021-01-11 18:52:14 +0100

    find bottleneck

--- Diagnostics.py
+++ Diagnostics.py
@@ -275,6 +275,7 @@ class Diagnostics:  # Make this a subclass of ht.DNDarray?
         flow_east[:, :-1]  = ht.clip(overland_flow_x[:, :-1], a_min=0, a_max=None)
         flow_east[:, :-1] -= ht.clip(-1 * overland_flow_x[:, 1:], a_min=0, a_max=None)
         flow_east[:, self.Nx-1] = ht.where(overland_flow_x[:,self.Nx-1]>0.0,overland_flow_x[:,self.Nx-1],Nix[:,self.Nx-1])
+        printroot('flow_east', flush=True)
 
         # for i in range (self.Nx-1):
         #     flow_east[:,i]  = ht.maximum(overland_flow_x[:,i], Nix[:,i])
@@ -286,6 +287,7 @@ class Diagnostics:  # Make this a subclass of ht.DNDarray?
         flow_west[:, 1:]  = ht.clip(overland_flow_x[:, :-1], a_min=0, a_max=None)
         flow_west[:, 1:] -= ht.clip(-1 * overland_flow_x[:, 1:], a_min=0, a_max=None)
         flow_west[:, 0] = ht.where(overland_flow_x[:,0]<0.0, overland_flow_x[:,0],Nix[:,0])
+        printroot('flow_west', flush=True)
         # for i in range (1,self.Nx):
         #     flow_west[:,i]  = ht.maximum(overland_flow_x[:,i-1], Nix[:,i-1])
         #     flow_west[:,i] -= ht.maximum((-1.0)*overland_flow_x[:,i], Nix[:,i])
@@ -296,6 +298,7 @@ class Diagnostics:  # Make this a subclass of ht.DNDarray?
         flow_north[:-1]  = ht.clip(overland_flow_y[:-1], a_min=0, a_max=None)
         flow_north[:-1] -= ht.clip(-1 * overland_flow_y[1:], a_min=0, a_max=None)
         flow_north[self.Ny-1] = ht.where(overland_flow_y[self.Ny-1,:]>0.0, overland_flow_y[self.Ny-1,:],Nix[self.Ny-1,:])
+        printroot('flow_north', flush=True)
         # for j in range (self.Ny-1):
         #     flow_north[j,:]  = ht.maximum(overland_flow_y[j,:], Nix[j,:])
         #     flow_north[j,:] -= ht.maximum((-1.0)*overland_flow_y[j+1,:], Nix[j+1,:])
@@ -306,6 +309,7 @@ class Diagnostics:  # Make this a subclass of ht.DNDarray?
         flow_south[1:]  = ht.clip(overland_flow_y[:-1], a_min=0, a_max=None)
         flow_south[1:] -= ht.clip(-1 * overland_flow_y[1:], a_min=0, a_max=None)
         flow_south[0] = ht.where(overland_flow_y[0,:]<0, overland_flow_y[0,:], Nix[0,:])
+        printroot('flow_south', flush=True)
         # for j in range (1,self.Ny):
         #     flow_south[j,:]  = ht.maximum(overland_flow_y[j-1,:], Nix[j-1,:])
         #     flow_south[j,:] -= ht.maximum((-1.0)*overland_flow_y[j,:], Nix[j,:])
