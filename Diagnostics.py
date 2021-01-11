9753a1ed3bf817d26210ebc4caffd8c02d48f1f0
Ben Bourgart <b.bourgart@fz-juelich.de>
2021-01-11 16:50:17 +0100

    find bug

--- Diagnostics.py
+++ Diagnostics.py
@@ -237,7 +237,8 @@ class Diagnostics:  # Make this a subclass of ht.DNDarray?
         return(subsurface_storage)
 
     def _TopLayerPressure(self, Press, fill_val=99999.0):
-        toplayer = ( (self.Mask > 0) * ht.arange(1, 1+self.Nz, dtype=ht.long)[:, None, None] ).max(0) -1
+        toplayer = ( (self.Mask > 0) * ht.arange(1, 1+self.Nz, dtype=ht.long)[:, None, None] ).max(0, keepdim=True) -1
+        toplayer = toplayer[0]
         toplayer.balance_()
         # toplayer contains the index of the highest layer and -1 if there is no highest layer
         y, x = np.indices((self.Ny, self.Nx), sparse=True)  # sparse=True is important, otherwise x, y are unsplit(numpy) and of shape2D -> memory
