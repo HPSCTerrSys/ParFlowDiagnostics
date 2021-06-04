3f7c891872998c9bf74b9f850a77bdd3444f4fab
Benjamin Bourgart <bourgart1@jwlogin08.juwels>
2021-06-04 14:06:36 +0200

    add outputs

---
+++ heat_tests.sh
@@ -0,0 +1,32 @@
+#!/bin/bash -x
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --time=00:15:00
+#SBATCH --partition=batch
+#SBATCH --account=slts
+#SBATCH --exclusive
+#SBATCH --output=serial.out
+#SBATCH --error=serial.err
+export OMP_NUM_THREADS=24
+
+echo test1
+srun python test1.py
+
+#srun python drainageslab.py
+
+#srun python inflowslab1
+echo
+echo parkinglot
+srun python parkinglot.py
+
+echo
+echo profileclm
+srun python profileclm.py
+
+echo
+echo slab
+srun python slab.py
+
+echo
+echo terrainfollwing
+srun python terrainfollowing.py
